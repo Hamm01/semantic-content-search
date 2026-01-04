bf2836e4fd2e1b77d443b4797ecaacc58c42b439
Himanish kochhar <himanish.kochhar@gmail.com>
2026-01-04 17:44:31 +0530

    updated the db.ts and drizzle config file setuped

--- src/app/db/db.ts
+++ src/app/db/db.ts
@@ -1,4 +1,4 @@
 import { drizzle } from 'drizzle-orm/neon-http'
 import { serverEnv } from '../data/serverEnv'
-
-export const db = drizzle(serverEnv.DATABASE_URL)
+import { relations } from './relations'
+export const db = drizzle(serverEnv.DATABASE_URL, { relations })
